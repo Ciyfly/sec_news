97c87a2424e003aae3b8f91049ca32dbd8e05909
Recar <1766241489@qq.com>
2021-01-15 08:47:16 +0800

    add 优化html增加分页

--- app/router.py
+++ app/router.py
@@ -4,13 +4,15 @@
 Author: Recar
 Date: 2021-01-10 23:04:42
 LastEditors: Recar
-LastEditTime: 2021-01-12 23:53:42
+LastEditTime: 2021-01-14 23:48:18
 '''
 from . import app
 from flask import render_template
 from app.models import News
 
 @app.route("/")
-def index():
-    datas = News.query.all()
-    return render_template("index.html", datas=datas)
+@app.route("/list/<int:page>")
+def index(page=1):
+    pagination = News.query.order_by(News.update_time.desc()).paginate(page=page, per_page=20)
+    datas = pagination.items
+    return render_template("index.html", datas=datas, pagination=pagination)
