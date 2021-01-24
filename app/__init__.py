b6cac6423f6871a6fffb6f8c8b9eb06200b175d5
Recar <1766241489@qq.com>
2021-01-24 18:39:15 +0800

    fix 增加db初始化

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@
 Author: Recar
 Date: 2021-01-12 23:38:09
 LastEditors: Recar
-LastEditTime: 2021-01-16 11:17:45
+LastEditTime: 2021-01-24 18:37:09
 '''
 
 from flask import Flask
@@ -18,5 +18,8 @@ app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + os.path.join(base_dir, ".
 app.config["SQLALCHEMY_COMMIT_ON_TEARDOWN"] = True
 
 db = SQLAlchemy(app)
-
 from app import router
+
+with app.app_context(): # 添加这一句，否则会报数据库找不到application和context错误
+    db.init_app(app) # 初始化db
+    db.create_all() # 创建所有未创建的table
