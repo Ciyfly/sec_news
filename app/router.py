c79a814345c599fd6faa16888eb153eec96bfab0
Ciyfly <1766241489@qq.com>
2021-05-24 16:30:51 +0800

    修改展示样式

--- app/router.py
+++ app/router.py
@@ -3,32 +3,36 @@
 '''
 Author: Recar
 Date: 2021-01-10 23:04:42
-LastEditors: Recar
-LastEditTime: 2021-01-24 15:52:07
+LastEditors: recar
+LastEditTime: 2021-05-24 11:38:58
 '''
 from . import app
 from flask import render_template
 from app.models import News, Company, Domain
+import datetime
 
 @app.route("/")
 @app.route("/list/<int:page>")
 def index(page=1):
     pagination = News.query.filter(News.new_type==0).order_by(News.update_time.desc()).paginate(page=page, per_page=20)
     datas = pagination.items
-    return render_template("index.html", datas=datas, pagination=pagination)
+    current_time = datetime.datetime.now()
+    return render_template("index.html", datas=datas, pagination=pagination, current_time=current_time)
 
 @app.route("/loophole")
 @app.route("/loophole/<int:page>")
 def loophole(page=1):
     pagination = News.query.filter(News.new_type==1).order_by(News.update_time.desc()).paginate(page=page, per_page=20)
     datas = pagination.items
-    return render_template("loophole.html", datas=datas, pagination=pagination)
+    current_time = datetime.datetime.now()
+    return render_template("loophole.html", datas=datas, pagination=pagination, current_time=current_time)
 
 @app.route("/subdomain")
 @app.route("/subdomain/<int:page>")
 def subdomain(page=1):
     companys = Company.query.all()
-    return render_template("subdomain.html", companys=companys)
+    current_time = datetime.datetime.now()
+    return render_template("subdomain.html", companys=companys, current_time=current_time)
 
 
 @app.route("/company/")
